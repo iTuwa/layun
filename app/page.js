6f4b4c0bfe29c36828f66b32a237ca19b2554152
Techie_Steve <tuwa.1simon@gmail.com>
2025-11-14 19:44:54 +0100

    Redirect HomePage to index.html

--- app/page.js
+++ app/page.js
@@ -1,9 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function HomePage() {
-  return (
-    <main style={{padding: 24}}>
-      <h1>Next.js migration scaffold</h1>
-      <p>Root page is up. UI migration will move your existing HTML/CSS/JS next.</p>
-      <p>Test the proxy route at <code>/route/secureproxy?e=ping_proxy</code> once dev server is running.</p>
-    </main>
-  );
+  redirect('/index.html');
 }
